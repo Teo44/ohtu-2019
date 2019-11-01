a36f0d5b826635b4fb0ca57d7db4b5c3a34e1f4c
Teo44 <43036149+Teo44@users.noreply.github.com>
2019-11-01 17:11:40 +0200

    viikko2 tehtava 1

---
+++ viikko2/tehtava1/src/main/java/ohtu/Multiplier.java
@@ -0,0 +1,14 @@
+package ohtu;
+
+public class Multiplier	{
+	private int value;
+	
+	public Multiplier(int v)	{
+		this.value = v;
+	}
+
+	public int multipliedBy(int other)	{
+		return value * other;
+	}
+
+}
