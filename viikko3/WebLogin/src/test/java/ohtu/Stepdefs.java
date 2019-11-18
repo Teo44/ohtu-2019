77a84c6fd091c780031bfd85fed86bba273e49ce
Teo44 <43036149+Teo44@users.noreply.github.com>
2019-11-18 13:16:29 +0200

    viikko 3 tehtava 10 (edellinen olikin 9)

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -23,6 +23,23 @@ public class Stepdefs {
         element.click();   
     }    
     
+    @Given("command new user is selected")
+    public void newUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+    }
+    
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUsernameAndPasswordAndConfirmationAreGiven(String username, String password)    {
+        registerWith(username, password, password);
+    }
+    
+    @When("an invalid username {string} and password {string} and matching password confirmation are entered")
+    public void invalidUsernameAndValidPasswordAndConfirmationAreGiven(String username, String password)    {
+        registerWith(username, password, password);
+    }
+    
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
@@ -31,6 +48,16 @@ public class Stepdefs {
     @When("nonexistant username {string} and password {string} are given")
     public void nonExistantUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
+    }
+    
+    @When("a valid username {string} and an invalid password {string} and a matching password confirmation are entered")
+    public void validUsernameAndInvalidPasswordAreGiven(String username, String password) {
+        registerWith(username, password, password);
+    }
+    
+    @When("a valid username {string} and password {string} and a mismatched password confirmation {string} are entered")
+    public void validUserNameAndMismatchedPasswordConfirmationAreGiven(String username, String password, String passwordConfirmation)   {
+        registerWith(username, password, passwordConfirmation);
     }
     
     @Then("user is logged in")
@@ -59,6 +86,17 @@ public class Stepdefs {
         assertTrue(driver.getPageSource().contains(pageContent));
     }
     
+    @Then("a new user is created")
+    public void successfulRegistering() {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+    
+    @Then("user is not created and error {string} is reported")
+    public void registeringFailsWithError(String errormessage)    {
+        pageHasContent("Create username and give password");
+        pageHasContent(errormessage);
+    }
+    
     @After
     public void tearDown(){
         driver.quit();
@@ -79,4 +117,15 @@ public class Stepdefs {
         element = driver.findElement(By.name("login"));
         element.submit();  
     } 
+    
+    private void registerWith(String username, String password, String passwordConfirmation)    {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passwordConfirmation);
+        element.submit();
+    }
 }
